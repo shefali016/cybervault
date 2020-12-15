26ffeb3c84ba37feaf995517631be6434048bbe6
Akhilesh Mourya <akhilmorya1206@gmail.com>
2020-12-15 18:41:56 +0530

    navigation handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,17 @@
 import "./App.css";
-import React, { useEffect, useState }from "react";
-import { connect } from 'react-redux';
 import { initFirebase } from "./firebaseConfig";
-import { Router, Switch, Route } from "react-router-dom";
-import LoginScreen from "./screens/LoginScreen";
-import SignUpScreen from "./screens/SignUpScreen";
-import { createBrowserHistory } from "history";
-import HomeScreen from "./screens/HomeScreen";
+import { Router } from "react-router-dom";
+import Routes from "./routes/navigationRoutes";
+import history from "./services/history"
+import React from "react";
 
 initFirebase();
-const rootHistory = createBrowserHistory();
 
 function App() {
-  const [state, setState] = useState(false);
-  const auth = state;
-  const handleLogin = (auth: boolean) => {
-    setState(state => ( auth ));
-  };
-
   return (
-
-    <Router history={rootHistory}>
-      <Switch>
-        <Route
-          path="/"
-          exact
-          render={(props: any) => (
-            <LoginScreen
-              {...props}
-              onLogin={handleLogin}
-            />
-          )}
-        />
-        <Route path="/signup" component={SignUpScreen} />
-        <Route path="/home" component={HomeScreen} />
-      </Switch>
+    <Router history={history}>
+      <Routes />
     </Router>
   );
 }
-const mapStateToProps = (state: any) => ({
-  isLoggedIn: state.auth.isLoggedIn,
-})
-
-export default connect(
-  mapStateToProps,
-  null,
-)(App)
+export default App
---
+++ src/routes/navigationRoutes.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState }from "react";
+import { Switch } from 'react-router-dom';
+import Route from "./index";
+import { connect } from 'react-redux';
+import LoginScreen from "../screens/LoginScreen";
+import SignUpScreen from "../screens/SignUpScreen";
+import HomeScreen from "../screens/HomeScreen";
+
+ function Routes(): JSX.Element {
+   
+  return (
+    <Switch>
+        <Route
+          path="/"
+          exact
+          component={LoginScreen}
+              
+              
+            
+        />
+        <Route path="/signup" component={SignUpScreen} />
+        <Route path="/home" component={HomeScreen} />
+        <Route component={LoginScreen}/>
+      </Switch>
+  );
+}
+const mapStateToProps = (state: any) => ({
+    isLoggedIn: state.auth.isLoggedIn,
+  })
+  
+  export default connect(
+    mapStateToProps,
+    null,
+  )(Routes)
