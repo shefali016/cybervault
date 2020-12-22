334c390f00201ad15f3e5640389ebf93ea7a192b
Akhilesh Mourya <akhilmorya1206@gmail.com>
2020-12-22 17:53:50 +0530

    WIP: Code structure improvements  3

--- src/components/Cards/PendingInvoices/index.tsx
+++ src/components/Cards/PendingInvoices/index.tsx
@@ -1,21 +1,53 @@
 import React from 'react';
 import { LinearProgress, Card, CardContent } from "@material-ui/core";
-import { Title, BodyText } from "./style";
+import { makeStyles } from '@material-ui/core/styles';
+import { CENTER, GRID } from 'utils/constants/stringConstants';
+import { BLACK_COLOR } from 'utils/constants/colorsConstants';
 
-const PendingInvoices = (props: {
+function PendingInvoices(props: {
     unpaidAmount?: number,
     paidAmount?: number
-}): JSX.Element => (
-
-        <Card style={{ display: "grid", width: '10rem', height: '10rem', borderRadius: 15 }}>
+}) {
+    const classes = useStyles();
+      return  (<Card className={classes.card}>
             <CardContent>
-                <Title> Invoices Pending</ Title>
-                <BodyText style={{ marginTop: 8 }}> $ {props.unpaidAmount ? props.unpaidAmount : 0}  Unpaid</BodyText>
-                <LinearProgress variant="determinate" value={props.unpaidAmount ? props.unpaidAmount : 50} style={{ width: 120, height: 18, borderRadius: 25, alignSelf: "center", marginTop: 8 }} />
-                <BodyText style={{ marginTop: 8 }}> $ {props.paidAmount ? props.paidAmount : 0}  Paid</BodyText>
-                <LinearProgress variant="determinate" value={props.paidAmount ? props.paidAmount : 50} style={{ width: 120, height: 18, borderRadius: 25, alignSelf: "center", marginTop: 8 }} />
+                <h5 className={classes.Title}> Invoices Pending</ h5>
+                <h6 className={classes.BodyText} > $ {props.unpaidAmount ? props.unpaidAmount : 0}  Unpaid</h6>
+                <LinearProgress className={classes.progressBar} variant="determinate" value={props.unpaidAmount ? props.unpaidAmount : 50}  />
+                <h6 className={classes.BodyText}> $ {props.paidAmount ? props.paidAmount : 0}  Paid</h6>
+                <LinearProgress className={classes.progressBar} variant="determinate" value={props.paidAmount ? props.paidAmount : 50}/>
             </CardContent>
-        </Card>
-    )
+        </Card>);
+}
+    const useStyles = makeStyles((theme) => ({
+        card: {
+            display: GRID, 
+            width: '10rem',
+            height: '10rem',
+            borderRadius: 15
+        },
+        progressBar: { 
+             width: 120, 
+             height: 18, 
+             borderRadius: 25, 
+             alignSelf: CENTER, 
+             marginTop: 8 
+        },
+        Title:
+        {
+             fontSize: '14px',
+             color: BLACK_COLOR,
+             fontWeight: 600,
+             margin: 0
+        },
+        BodyText:
+        {
+             fontSize: '12px',
+             color: BLACK_COLOR,
+             margin: 0,
+             marginTop: 8,
+        }
+
+   }));
 
 export default PendingInvoices;
--- src/components/Cards/UnpaidInvoices/index.tsx
+++ src/components/Cards/UnpaidInvoices/index.tsx
@@ -1,25 +1,28 @@
 import React from "react";
 import { Button, Card, CardContent, Typography } from "@material-ui/core";
 import logo from '../../../assets/logo.png';
+import { makeStyles } from '@material-ui/core/styles';
+import { BLOCK, BOLD, CENTER, GRID, FLEX, AUTO } from "utils/constants/stringConstants";
+import { GREY_COLOR, WHITE_COLOR } from "utils/constants/colorsConstants";
 
-export const UnpaidInvoices = (props: {
+function UnpaidInvoices(props: {
   projectDetails?: any,
   openProject?: any,
 
-}): JSX.Element => {
-
-  return (<Button onClick={props.openProject} style={{ height: '4rem', marginTop: 10, background: '#141814' }}>
-    <Card style={{ width: '16rem', height: '4rem', background: '#141814', zIndex: 2 }}>
+}) {
+  const classes = useStyles();
+  return (<Button onClick={props.openProject} className={classes.button}>
+    <Card className={classes.card}>
       <div>
         <CardContent style={{ display: 'flex' }}>
-            <div style={{ backgroundColor: 'white', borderRadius: 10, alignItems: 'center', justifyContent: 'center', display: 'flex', padding: 5, paddingTop: 10, paddingBottom: 10 }}>
-              <img src={(props.projectDetails && props.projectDetails.image) ? props.projectDetails.image : logo} style={{ width: "auto", height: "auto", display: "block", maxWidth:30, maxHeight:30 }} alt="Logo" />
+            <div className={classes.imgWrapper}>
+              <img className={classes.img} src={(props.projectDetails && props.projectDetails.image) ? props.projectDetails.image : logo} alt="Logo" />
             </div>
-            <div style={{ display: 'grid', marginLeft: 5, alignContent: 'center' }}>
-              <Typography style={{ fontWeight: "bold", fontSize: 8, color: '#FFF' }}>
+            <div className={classes.textWrapper}>
+              <Typography className={classes.bodyText}>
                 {(props.projectDetails && props.projectDetails.name) ? props.projectDetails.name : "Nike Summer Campaign"}
               </Typography>
-              <Typography style={{ fontWeight: "bold", fontSize: 6, color: 'grey' }}>
+              <Typography className={classes.bottomText}>
                 {(props.projectDetails && props.projectDetails.value) ? props.projectDetails.value : "Doc 2016 campaign with audi Q6"}
               </Typography>
             </div>
@@ -28,4 +31,51 @@ export const UnpaidInvoices = (props: {
     </Card>
   </Button>);
 }
+const useStyles = makeStyles((theme) => ({
+  button:{
+    height: '4rem', 
+    marginTop: 10, 
+    background: '#141814',
+  },
+  card: {
+    width: '16rem', 
+    height: '4rem', 
+    background: '#141814', 
+    zIndex: 2,
+  },
+  imgWrapper: { 
+    backgroundColor: WHITE_COLOR, 
+    borderRadius: 10, 
+    alignItems: CENTER, 
+    justifyContent: CENTER, 
+    display: FLEX, 
+    padding: 5, 
+    paddingTop: 10, 
+    paddingBottom: 10  
+  },
+  bodyText:
+  {
+    fontWeight: BOLD, 
+    fontSize: 8, 
+    color: '#FFF',
+  },
+ img: { 
+    width: AUTO, 
+    height: AUTO, 
+    display: BLOCK, 
+    maxWidth:30, 
+    maxHeight:30 
+  },
+  textWrapper: {
+    display: GRID, 
+    marginLeft: 5, 
+    alignContent: CENTER,
+  },
+  bottomText:{
+    fontWeight: BOLD, 
+    fontSize: 6, 
+    color: GREY_COLOR,
+  }
+
+}));
 export default UnpaidInvoices;
