96a16399a57921e280206247c6ef608924133777
Morgan Trudeau <morgantrudeau@gmail.com>
2021-01-31 21:21:51 -0800

    Refactor Carousel name

--- src/components/Assets/UploadMedia.tsx
+++ src/components/Assets/UploadMedia.tsx
@@ -1,7 +1,7 @@
 import { Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import clsx from 'clsx'
-import { AssetCarousel } from 'components/Common/Carousel'
+import { AssetCarousel } from 'components/Common/Carousel/Carousel'
 import { DragAndDropUploader } from 'components/Common/DragAndDropFileUpload'
 
 type AssetUploadDisplayProps = {
